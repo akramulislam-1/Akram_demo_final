479add364a4763d0f9400c37abc1bac7c741ac8c
akram <akram29031999@gmail.com>
2022-09-01 15:26:34 +0530

    Add project files.

---
+++ Akram_demo_final/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Akram_demo_final.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
